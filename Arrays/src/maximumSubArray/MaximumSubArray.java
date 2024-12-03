6b5e39b12e768996ce3dc591a1e97b238bece017
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2024-12-04 00:30:50 +0530

    LeetCode #53: Added code for Maximum Sum SubArray

--- Arrays/src/maximumSubArray/MaximumSubArray.java
+++ Arrays/src/maximumSubArray/MaximumSubArray.java
@@ -1,2 +1,20 @@
-package maximumSubArray;public class MaximumSubArray {
+package maximumSubArray;
+
+public class MaximumSubArray {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+            currentSum = currentSum + num;
+            if (currentSum > sum) {
+                sum = currentSum;
+            }
+        }
+
+        return sum;
+    }
 }
