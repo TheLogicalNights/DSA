ac4ab669ca265162cf812a190bd9f4c363fe5de0
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2024-12-19 22:51:45 +0530

    LeetCode #152: Added code for Maximum Product SubArray

---
+++ Arrays/src/maximumProductSubArray/MaximumProductSubArray.java
@@ -0,0 +1,24 @@
+package maximumProductSubArray;
+
+public class MaximumProductSubArray {
+    public int maxProduct(int[] nums) {
+        int result = nums[0];
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+                minProduct = Math.min(nums[i], minProduct * nums[i]);
+            } else {
+                int temp = maxProduct;
+                maxProduct = Math.max(nums[i], minProduct * nums[i]);
+                minProduct = Math.min(nums[i], temp * nums[i]);
+            }
+
+            result = Math.max(result, maxProduct);
+        }
+
+        return result;
+    }
+}
