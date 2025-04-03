6c997055c8e45c477afff15ba9371af812a69ba5
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2025-04-04 01:00:05 +0530

    Added code to remove last node from a linkedList

--- LinkedList/src/LinkedList/LinkedList.java
+++ LinkedList/src/LinkedList/LinkedList.java
@@ -11,6 +11,10 @@ public class LinkedList {
             this.data = data;
             this.next = null;
         }
+
+        public int getData() {
+            return this.data;
+        }
     }
 
     public LinkedList() {
@@ -27,6 +31,23 @@ public class LinkedList {
             this.tail.next = newNode;
             this.tail = newNode;
         }
+    }
+
+    public Node removeLast() {
+        Node temp = this.head;
+        Node pre = this.head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            if (temp.next != null) {
+                pre = pre.next;
+            }
+        }
+
+        this.tail = pre;
+        this.tail.next = null;
+
+        return temp;
     }
 
     public void printList() {
--- LinkedList/src/LinkedList/Main.java
+++ LinkedList/src/LinkedList/Main.java
@@ -3,10 +3,20 @@ package LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+        // Test add function
         linkedList.add(1);
         linkedList.add(2);
         linkedList.add(3);
         linkedList.add(4);
+
+        // Test printList function
         linkedList.printList();
+
+        // Test removeLast function
+        LinkedList.Node lastElement = linkedList.removeLast();
+        System.out.println("\nDeleted Node: " + lastElement.getData());
+        linkedList.printList();
+
+
     }
 }
